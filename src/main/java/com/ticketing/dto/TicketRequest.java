bb42d256e5c9be70269bff1a1620f2eb33f8eed3
Hilal <hilalayseakgul@gmail.com>
2026-09-19 22:13:03 +0300

    fix: Add @Builder annotation to TicketRequest DTO to fix VS Code compiler error

--- src/main/java/com/ticketing/dto/TicketRequest.java
+++ src/main/java/com/ticketing/dto/TicketRequest.java
@@ -1,10 +1,12 @@
 package com.ticketing.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class TicketRequest {
